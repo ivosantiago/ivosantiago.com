c0389c1792aaae82e61c0c60b25278003075b746
Ivo Santiago <ivo.santiago@appcues.com>
2023-01-05 11:09:25 -0300

    remove fake content from articles and projects page

--- app/routes/__app/articles.tsx
+++ app/routes/__app/articles.tsx
@@ -44,8 +44,8 @@ export default function ArticlesIndex() {
         />
       </Head> */}
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on software engineer and life."
+        intro="All of my long-form thoughts on programming, leadership, and more, collected in chronological order. The idea here is to write notes to me, and share them with you."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
--- app/routes/__app/projects.tsx
+++ app/routes/__app/projects.tsx
@@ -6,42 +6,42 @@ import logoHelioStream from "~/images/logos/helio-stream.svg";
 import logoOpenShuttle from "~/images/logos/open-shuttle.svg";
 import logoPlanetaria from "~/images/logos/planetaria.svg";
 
-const projects = [
-  {
-    name: "Planetaria",
-    description:
-      "Creating technology to empower civilians to explore space on their own terms.",
-    link: { href: "http://planetaria.tech", label: "planetaria.tech" },
-    logo: logoPlanetaria,
-  },
-  {
-    name: "Animaginary",
-    description:
-      "High performance web animation library, hand-written in optimized WASM.",
-    link: { href: "#", label: "github.com" },
-    logo: logoAnimaginary,
-  },
-  {
-    name: "HelioStream",
-    description:
-      "Real-time video streaming library, optimized for interstellar transmission.",
-    link: { href: "#", label: "github.com" },
-    logo: logoHelioStream,
-  },
-  {
-    name: "cosmOS",
-    description:
-      "The operating system that powers our Planetaria space shuttles.",
-    link: { href: "#", label: "github.com" },
-    logo: logoCosmos,
-  },
-  {
-    name: "OpenShuttle",
-    description:
-      "The schematics for the first rocket I designed that successfully made it to orbit.",
-    link: { href: "#", label: "github.com" },
-    logo: logoOpenShuttle,
-  },
+const projects: any[] = [
+  // {
+  //   name: "Planetaria",
+  //   description:
+  //     "Creating technology to empower civilians to explore space on their own terms.",
+  //   link: { href: "http://planetaria.tech", label: "planetaria.tech" },
+  //   logo: logoPlanetaria,
+  // },
+  // {
+  //   name: "Animaginary",
+  //   description:
+  //     "High performance web animation library, hand-written in optimized WASM.",
+  //   link: { href: "#", label: "github.com" },
+  //   logo: logoAnimaginary,
+  // },
+  // {
+  //   name: "HelioStream",
+  //   description:
+  //     "Real-time video streaming library, optimized for interstellar transmission.",
+  //   link: { href: "#", label: "github.com" },
+  //   logo: logoHelioStream,
+  // },
+  // {
+  //   name: "cosmOS",
+  //   description:
+  //     "The operating system that powers our Planetaria space shuttles.",
+  //   link: { href: "#", label: "github.com" },
+  //   logo: logoCosmos,
+  // },
+  // {
+  //   name: "OpenShuttle",
+  //   description:
+  //     "The schematics for the first rocket I designed that successfully made it to orbit.",
+  //   link: { href: "#", label: "github.com" },
+  //   logo: logoOpenShuttle,
+  // },
 ];
 
 function LinkIcon(props: React.SVGProps<SVGSVGElement>) {
@@ -66,13 +66,10 @@ export default function Projects() {
         />
       </Head> */}
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        title="Things I’ve made to have fun."
+        intro="These are things that I do for fun and to learn. I’m always looking for new projects to work on, so if you have any ideas, let me know!"
       >
-        <ul
-          role="list"
-          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-        >
+        <ul className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
           {projects.map((project) => (
             <Card as="li" key={project.name}>
               <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
