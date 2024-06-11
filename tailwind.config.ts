5ef205cbaed07bf24abb72b082741fef5da4cc65
Ivo Santiago <ivo.santiago@appcues.com>
2024-06-10 21:13:11 -0300

    starting from nextjs default

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
