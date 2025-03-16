fcc14e485bc33c661978c4aba0c0566eecbd683b
Ivo Santiago <ivo.santiago@appcues.com>
2025-03-16 15:15:16 -0300

    nextjs update

--- mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import Image, { type ImageProps } from 'next/image'
-import { type MDXComponents } from 'mdx/types'
-
-export function useMDXComponents(components: MDXComponents) {
-  return {
-    ...components,
-    Image: (props: ImageProps) => <Image {...props} />,
-  }
-}
